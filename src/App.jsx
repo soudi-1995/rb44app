7b06c81860f2b651e9102e147d711b040bf4107d
soudi-1995 <irukuvajjhulasss@gmail.com>
2025-02-15 10:53:43 +0530

    basic

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-success'>Register here</h1>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
