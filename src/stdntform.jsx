e0d86e4ae386eccd152c7278ead1fe2af034098a
soudi-1995 <irukuvajjhulasss@gmail.com>
2025-02-19 15:06:41 +0530

    changes

--- src/stdntform.jsx
+++ src/stdntform.jsx
@@ -11,7 +11,7 @@ function StudentForm() {
             gender:''
         },
         onSubmit:(values)=>{
-            alert("")
+            
             console.log(values)
         }
     })
@@ -26,11 +26,18 @@ function StudentForm() {
         <br />
         <input type="text" name='age' onChange={studentForm.handleChange} />
         <br />
+        <h6>Gender:</h6>
         <input type="radio" name='gender' value='Female' onChange={studentForm.handleChange} />Female
         <input type="radio" name='gender' value='Female' onChange={studentForm.handleChange} />Male
         <input type="radio" name='gender' value='Female' onChange={studentForm.handleChange} />Others
         <br />
-        <button>Save Details</button>
+        <h6>Skills:</h6>
+        <input type="checkbox" name='skill' value='ReactJS' onChange={studentForm.handleChange}/>REACtJS
+        <input type="checkbox" name='skill' value='DevOps' onChange={studentForm.handleChange}/>DevOps
+        <input type="checkbox" name='skill' value='HTML/CSS' onChange={studentForm.handleChange}/>HTML/CSS
+        <input type="checkbox" name='skill' value='NOdeJS' onChange={studentForm.handleChange}/>NOdeJS
+        <br />
+        <button>Save Details</button> 
         </form>
         </div>
     ) 
