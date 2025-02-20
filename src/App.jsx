079805b3d623c8ff087669af2254293848eb4ffc
soudi-1995 <irukuvajjhulasss@gmail.com>
2025-02-20 09:55:59 +0530

    addede adrs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef} from 'react'
+import Address from './address'
 
 
 function App() {
+  var ref1 = useRef()
+  var ref2 = useRef()
+  useEffect(()=>{ ref1.current.focus();
+                
+   },[])
+  
+
+  function Enter(e){
+      if(e.key==='Enter'){
+        ref2.current.focus()
+      }
+  }
   return (
     <div>
       <h1 className='bg-success'>New to useRef</h1>
+      Enter your FirstName: <input type="text" ref={ref1} onKeyUp={(e)=>{Enter(e)}} />
+      Enter your LastName: <input type="text" ref={ref2} />
+      <Address></Address>
     </div> 
   );
 } 
---
+++ src/address.jsx
@@ -0,0 +1,8 @@
+function Address(){
+    return (
+        <div className='border border-2 m-3 p-2'>
+            <input type="text" />
+        </div>
+    )
+}
+export default Address;
