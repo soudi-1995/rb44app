14e92aa67b67ca33b9a91ddb4fee1349c39cc873
soudi-1995 <irukuvajjhulasss@gmail.com>
2025-02-25 12:12:45 +0530

    deleted fis

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Address from './address'
 function App() {
   var ref1 = useRef()
   var ref2 = useRef()
+  var ref3 = useRef()
+  var ref4 = useRef()
   useEffect(()=>{ ref1.current.focus();
                 
    },[])
@@ -17,12 +19,22 @@ function App() {
         ref2.current.focus()
       }
   }
+  function Enter2(e){
+    if(e.key==='Enter'){
+      ref3.current.focus()
+    }
+  }
+  
   return (
     <div>
       <h1 className='bg-success'>New to useRef</h1>
-      Enter your FirstName: <input type="text" ref={ref1} onKeyUp={(e)=>{Enter(e)}} />
-      Enter your LastName: <input type="text" ref={ref2} />
-      <Address></Address>
+      Enter your FirstName: <input type="text" ref={ref1} onKeyUp={(e)=>{Enter(e)}} /> <br />
+      <br />
+      Enter your LastName: <input type="text" ref={ref2} onKeyUp={(e)=>{Enter2(e)}} />
+      <br />
+      <Address ref={[ref3,ref4]}></Address> <br />
+      <br />
+      PIN: <input type="text" ref ={ ref4}/>
     </div> 
   );
 } 
--- src/address.jsx
+++ src/address.jsx
@@ -1,8 +1,17 @@
-function Address(){
+import { forwardRef } from "react";
+
+var Address = forwardRef((props, refs)=>{
+    function Enter3(e){
+        if(e.key==='Enter'){
+          refs[1].current.focus()
+        }
+      }
     return (
-        <div className='border border-2 m-3 p-2'>
-            <input type="text" />
+        <div style={{border:'2px solid gray ' , margin:'5px' , padding:'10px'}}>
+            <br />
+          
+           Address: <input type="text" onKeyUp={(e)=>{Enter3(e)}} ref ={refs[0]}/>
         </div>
-    )
-}
+    )})
+
 export default Address;
