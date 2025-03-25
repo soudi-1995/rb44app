d1a29a548ebe8a904fcc3e5b026c413db25fe2d3
soudi-1995 <irukuvajjhulasss@gmail.com>
2025-03-25 09:30:47 +0530

    issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_BOOKS = gql`
+
+                    query GetAllBooks {
+                          getAllBooks {
+                            title
+                            author
+                            id
+                            }
+                          }`
 
 function App() {
+  const {loading, error, data:{getAllBooks:books}} = useQuery(GET_BOOKS)
+   
   return (
     <div>
       <h1 className='bg-success'>Register here</h1>
       <h2>hello</h2>
-      <Counter></Counter>
+    
+      <ul>
+      { 
+       loading && <b>Loading...</b>
+      }
+      {
+        !loading && books?.map((book)=>{
+          return <li>{book.title}</li>
+        })
+         
+} 
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
+export const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-    <App />
+ <ApolloProvider client={client}>
+        <App />
+ </ApolloProvider>,
 
 );
 
