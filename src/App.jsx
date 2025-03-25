bd14c6765b38ef15382e51e3be0533b21b78ffa8
soudi-1995 <irukuvajjhulasss@gmail.com>
2025-03-25 12:59:41 +0530

    created graphql with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useMutation } from '@apollo/client';
+import { useState } from 'react';
 
 const GET_BOOKS = gql`
 
                     query GetAllBooks {
-                          getAllBooks {
-                            title
+                          getAllBooks {                           
                             author
                             id
+                            title
                             }
-                          }`
+                          }
+                            `;
+const  CREATE_BOOK =gql`
+              mutation CreateBook($title: String!, $author: String) {
+              createBook(title: $title, author: $author) {
+                title
+                author
+                id
+  }
+}
+`                      
 
 function App() {
-  const {loading, error, data:{getAllBooks:books}} = useQuery(GET_BOOKS)
-   
+
+  const {loading, error, data} = useQuery(GET_BOOKS)
+  const [addBookFn] = useMutation(CREATE_BOOK)
+  var [newBook, setNewBook] = useState({
+    author:"",
+    title:"",
+    id:""
+  })
+  function addBook(){
+    addBookFn({
+      variables:newBook
+    }).then((res)=>{
+      console.log(res)
+    })
+  }
+
   return (
     <div>
       <h1 className='bg-success'>Register here</h1>
-      <h2>hello</h2>
-    
+      <input type="text" onChange={(e)=>{setNewBook({...newBook,author:e.target.value})}} placeholder='author name' />
+      <br />
+      <input type="text" onChange={(e)=>{setNewBook({...newBook,title:e.target.value})}} placeholder='author title' />
+      <br />
+      <button onClick={addBook}>Add Book</button>
       <ul>
+        
       { 
        loading && <b>Loading...</b>
       }
       {
-        !loading && books?.map((book)=>{
-          return <li>{book.title}</li>
-        })
+        !loading && data?.getAllBooks.map((book)=>{
+          return ( <div>
+              <li> {`${book.id} . ${book.title}`}</li>
+             
+          </div> 
+        )})
          
-} 
+      } 
       </ul>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:4000/',
+    uri: 'http://localhost:4000',
     cache: new InMemoryCache(),
   });
 
